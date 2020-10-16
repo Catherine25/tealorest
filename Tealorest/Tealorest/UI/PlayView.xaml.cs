c68b685a750a11aa8fc48b8086496e020d3323da
Catherine <CatherineSolodoukhina@gmail.com>
2020-10-17 00:03:26 +0300

    Add PlayView

---
+++ Tealorest/Tealorest/UI/PlayView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+using Tealorest.Data;
+
+namespace Tealorest.UI
+{
+    public partial class PlayView : UserControl
+    {
+        public PlayView() => InitializeComponent();
+
+        public PlayView(Palette palette)
+        {
+            InitializeComponent();
+
+            xTopBorder.Fill = new SolidColorBrush(palette.Color1);
+            xBottomBorder.Fill = new SolidColorBrush(palette.Color1);
+        }
+    }
+}
