68c7c63ad5c889e9948a3b089618f670c5567a29
Catherine <CatherineSolodoukhina@gmail.com>
2020-10-18 14:21:17 +0300

    Fix spacing

--- Tealorest/Tealorest/MainWindow.xaml.cs
+++ Tealorest/Tealorest/MainWindow.xaml.cs
@@ -13,17 +13,17 @@ namespace Tealorest
 
             Palette palette = new Palette()
             {
-                LightestMain = Color.FromRgb(253,246,227),
-                LightestAdditional = Color.FromRgb(238,232,213),
+                LightestMain = Color.FromRgb(253, 246, 227),
+                LightestAdditional = Color.FromRgb(238, 232, 213),
 
-                LighterMiddleMain = Color.FromRgb(147,161,161),
-                LighterMiddleAdditional = Color.FromRgb(131,148,150),
+                LighterMiddleMain = Color.FromRgb(147, 161, 161),
+                LighterMiddleAdditional = Color.FromRgb(131, 148, 150),
 
-                DarkerMiddleMain = Color.FromRgb(88,110,117),
-                DarkerMiddleAdditional = Color.FromRgb(101,123,131),
+                DarkerMiddleMain = Color.FromRgb(88, 110, 117),
+                DarkerMiddleAdditional = Color.FromRgb(101, 123, 131),
 
-                DarkestMain = Color.FromRgb(0,43,54),
-                DarkestAdditional = Color.FromRgb(7,54,66)
+                DarkestMain = Color.FromRgb(0, 43, 54),
+                DarkestAdditional = Color.FromRgb(7, 54, 66)
             };
 
             Content = new PlayView(palette);
