fd3b0ed41a9d8fedaf10b3bcf6cb810842027791
Catherine <CatherineSolodoukhina@gmail.com>
2020-10-24 22:56:07 +0300

    Add Surface view

---
+++ Tealorest/Tealorest/UI/Surface.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Tealorest.UI
+{
+    public partial class Surface : UserControl
+    {
+        public Surface() => InitializeComponent();
+
+        public Surface(Size size)
+        {
+            InitializeComponent();
+
+            xImage.Width = size.Width;
+            xImage.Height = size.Height;
+        }
+    }
+}
