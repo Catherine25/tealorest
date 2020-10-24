53ffaed0ec6c8688237d8b240ebb7c16fe792a77
Catherine <CatherineSolodoukhina@gmail.com>
2020-10-24 22:53:26 +0300

    Add Extendions

---
+++ Tealorest/Tealorest/Data/Extensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Tealorest.Data
+{
+    public static class Extensions
+    {
+        public static Size GetSize(this FrameworkElement element) =>
+            new Size(element.ActualWidth, element.ActualHeight);
+
+        public static void SetSize(this FrameworkElement element, Size size)
+        {
+            element.Width = size.Width;
+            element.Height = size.Height;
+        }
+
+        public static void ModifyMarginLeft(this FrameworkElement element, double left)
+        {
+            Thickness margin = element.Margin;
+            margin.Left += left;
+            element.Margin = margin;
+        }
+
+        public static void ModifyMarginRight(this FrameworkElement element, double right)
+        {
+            Thickness margin = element.Margin;
+            margin.Right += right;
+            element.Margin = margin;
+        }
+
+        public static void ModifyMarginTop(this FrameworkElement element, double top)
+        {
+            Thickness margin = element.Margin;
+            margin.Top += top;
+            element.Margin = margin;
+        }
+
+        public static void ModifyMarginBottom(this FrameworkElement element, double bottom)
+        {
+            Thickness margin = element.Margin;
+            margin.Bottom += bottom;
+            element.Margin = margin;
+        }
+    }
+}
