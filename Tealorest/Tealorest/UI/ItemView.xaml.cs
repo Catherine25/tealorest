779538b0887d508c835a13bea05141ec01dd4b34
Catherine <CatherineSolodoukhina@gmail.com>
2020-10-30 18:59:42 +0200

    Add ItemView

---
+++ Tealorest/Tealorest/UI/ItemView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Tealorest.UI
+{
+    public partial class ItemView : UserControl
+    {
+        public ItemView() => InitializeComponent();
+
+        public ItemView(string name)
+        {
+            InitializeComponent();
+            SetSource(name);
+        }
+
+        private void SetSource(string name)
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, "Data", "Images", $"{name}.png");
+            xImage.Source = new BitmapImage(new Uri(path));
+        }
+    }
+}
